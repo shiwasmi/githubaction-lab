984437d6ca1e8822eff859f192029a76755a0ec0
shiwasmi <sagarchattar@gmail.com>
2026-04-25 14:46:58 +0530

    Project Updates

---
+++ src/main/java/com/example/githubaction_lab/GithubactionLabApplication.java
@@ -0,0 +1,13 @@
+package com.example.githubaction_lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubactionLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubactionLabApplication.class, args);
+	}
+
+}
